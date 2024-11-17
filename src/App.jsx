cd1c9ae903af3e206437b9796eb7b8375d921a13
hggnomm <hggnomm.dev@gmail.com>
2024-11-18 00:07:52 +0700

    Navigating between pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import ChatBotStart from "./components/ChatBotStart/ChatBotStart";
 import ChatBotApp from "./components/ChatBotApp/ChatBotApp";
 
 const App = () => {
+  const [isChatting, setIsChatting] = useState(false);
+
+  const handleStartChat = () => {
+    setIsChatting(true);
+  };
+
+  const handleGoBack = () => {
+    setIsChatting(false);
+  };
   return (
     <div className="container">
-      {/* <ChatBotStart /> */}
-      <ChatBotApp />
+      {!isChatting ? (
+        <ChatBotStart onStartChat={handleStartChat} />
+      ) : (
+        <ChatBotApp onGoBack={handleGoBack} />
+      )}
     </div>
   );
 };
